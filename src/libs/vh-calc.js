ca55fb02a94987b890951e21e37baf8472deb7b3
Behnam Azimi <bhnmzm@gmail.com>
2021-01-30 22:09:16 +0300

    vh bug fix

---
+++ src/libs/vh-calc.js
@@ -0,0 +1,12 @@
+// fix vh bug for mobile phones
+function calcVH() {
+    const windowHeight = (document
+        && document.documentElement
+        && document.documentElement.clientHeight) || window.innerHeight;
+
+    let vh = windowHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+
+
+window.addEventListener('resize', calcVH);
